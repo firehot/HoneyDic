2a01ee7c9c8ca575626afe58d64e6a7b0c331fc7
yagnu <ice3x2@gmail.com>
2015-05-06 05:09:26 +0900

    * issue #13 관련 : 깔쌈한 옵션 프레그먼트 추가.

--- app/src/main/java/kr/re/dev/MoongleDic/MainActivity.java
+++ app/src/main/java/kr/re/dev/MoongleDic/MainActivity.java
@@ -1,8 +1,25 @@
 package kr.re.dev.MoongleDic;
 
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
+
+import com.rey.material.drawable.CheckBoxDrawable;
+import com.rey.material.util.ThemeUtil;
+import com.rey.material.widget.Button;
+import com.rey.material.widget.CheckBox;
+import com.rey.material.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+
 import kr.re.dev.MoongleDic.DicService.PhoneticPlayer;
 import kr.re.dev.MoongleDic.UI.DicItemViewWrapper;
 
@@ -13,8 +30,21 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        finish();
+        setContentView(R.layout.fragment_option);
+        //CheckBox checkBox = (CheckBox)findViewById(R.id.viewCheckBoxUse);
+        Spinner spinner = (Spinner)findViewById(R.id.viewSpinnerKeepTime);
+
+
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.KeepWordCardTimeSelectSpinnerItem, R.layout.item_keep_wordcard);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((self_, view, pos, id) -> {
+
+
+        });
+
+        //finish();
         /*DicInfoManager dicInfoManager =  DicInfoManager.newInstance(this);
         DicInfoManager.DicInfo dicInfo =  dicInfoManager.getDicInfo(DicInfoManager.Dic.EnglishToKorean);
         item = DicItemViewWrapper.obtain(this);
@@ -22,6 +52,7 @@ public class MainActivity extends FragmentActivity {
         mPhoneticPlayer = PhoneticPlayer.newInstance(this, dicInfo.getFromLanguage());*/
 
     }
+
 
 
     @Override
---
+++ app/src/main/java/kr/re/dev/MoongleDic/UI/OptionFragment.java
@@ -0,0 +1,11 @@
+package kr.re.dev.MoongleDic.UI;
+
+import android.support.v4.app.Fragment;
+
+/**
+ *
+ * Created by ice3x2 on 15. 5. 5..
+ */
+public class OptionFragment extends Fragment{
+    
+}
